c1c24b3f8a3d81af769fb7a4efc5993bec7777b2
Noww_yun <leejaeyun159>
2021-03-23 21:52:06 +0900

    first test

---
+++ test.js
@@ -0,0 +1,39 @@
+// console.log("Hello world!");
+
+// let number = 1;
+// let text = "안녕하세요";
+
+// const constant = "test";
+
+// console.log(text);
+
+// let a = 1; 
+// if(a==1){
+//     console.log("a는1입니다");
+// }
+// else if(a==2){
+//     console.log("a는 2입니다");
+// }
+// else{
+//     console.log("숫자가 너무큽니다");
+// }
+
+// let a = 1+2;
+// let b = 2-1;
+// let c = 2*2;
+// let d = 3/2;
+//  let number = 1;
+//  let text = "1";
+
+//  console.log(number==text);
+//  console.log(number===text);
+
+//  for(let i = 0 ; i < 100 ; i ++ ){
+//      console.log(i);
+//  }
+
+let a = ['하나','둘','셋', ]
+
+for(let i=0;i<4;i++){
+    console.log(i);
+}
